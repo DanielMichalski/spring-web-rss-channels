fedf0afc7a24628c2eaa946014151c82f6a607d6
danielmichalski <michalskidaniel2@gmail.com>
2015-07-29 17:06:29 +0200

    Upgraded libraries versions

--- src/test/java/pl/dmichalski/rss/controller/UserControllerTest.java
+++ src/test/java/pl/dmichalski/rss/controller/UserControllerTest.java
@@ -1,6 +1,5 @@
 package pl.dmichalski.rss.controller;
 
-import netscape.security.Principal;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,18 +13,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.context.WebApplicationContext;
 import pl.dmichalski.rss.entity.RssFeedEntity;
 import pl.dmichalski.rss.service.IRssFeedService;
 import pl.dmichalski.rss.service.IUserService;
 
-import static org.junit.Assert.*;
-import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/web-mvc-config.xml",
